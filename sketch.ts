dcb294a1a68df8bb3bdcb46d80c0b2bce005059f
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-24 16:31:55 +0530

    created sketch

---
+++ sketch.ts
@@ -0,0 +1,17 @@
+/// <reference path="libs/p5.d.ts"/>
+/// <reference path="libs/p5.global-mode.d.ts"/>
+
+const W = window.innerWidth;
+const H = window.innerHeight;
+
+function preload() {
+
+}
+
+function setup() {
+    createCanvas(W, H);
+}
+
+function draw() {
+
+}
