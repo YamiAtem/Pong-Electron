bbfb7a2c71f715829c9f167d74b77df3f7dd1faf
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-24 17:15:11 +0530

    added play, computer, ball, edge sprites

--- src/sketch.js
+++ src/sketch.js
@@ -4,13 +4,33 @@
 const W = window.innerWidth;
 const H = window.innerHeight;
 
-var player_paddle, comp_paddle, ball;
+
+//player and computer
+var player, computer;
+
+//ball
+var ball;
+
+//edges
+var edges;
 
 function setup() {
     createCanvas(W, H);
 
-    // paddles
-    player_paddle = createSprite(770, 482-25, 10, 50);
+    //Player sprite
+    player = createSprite(796, 241 - 40, 20, 96.4);
+    player.shapeColor = "blue";
+
+    //Computer
+    computer = createSprite(60, 241 - 40, 20, 96.4);
+    computer.shapeColor = "red";
+
+    //ball
+    ball = createSprite(200, 200, 10, 10);
+    ball.shapeColor = "yellow";
+
+    //edges
+    edges = createEdgeSprites();
 }
 
 function draw() {
